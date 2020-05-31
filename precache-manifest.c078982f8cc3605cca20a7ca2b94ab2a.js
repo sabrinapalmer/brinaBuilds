05a214b53992fe4421197c08c0ff06cc5edc2b58
Sabrina <spalmer2@oxy.edu>
2020-05-30 19:39:46 -0500

    Updates

---
+++ precache-manifest.c078982f8cc3605cca20a7ca2b94ab2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43bcfe29b3dffe4e2da688590234e95a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b1e475436e2bdad8a6b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36b72a3501bfc2f536a1",
+    "url": "/static/js/2.a4d51e45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4d51e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1e475436e2bdad8a6b",
+    "url": "/static/js/main.bbd34a81.chunk.js"
+  },
+  {
+    "revision": "cb52fec2129a4fe5209e",
+    "url": "/static/js/runtime-main.84dae810.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
